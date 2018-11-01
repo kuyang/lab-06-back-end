8a76575e52644f55345cf86b589436f89bf6d2dc
Greg Hsieh <greg.hsieh@gmail.com>
2018-10-31 20:09:55 -0400

    feature 1 is working

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 'use strict'
 
+require('dotenv').config();
+
 const cors = require('cors');
 const express = require('express');
 const superagent = require('superagent');
-
-require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT||3000;
 
+app.use(cors())
 
-app.get('/',(request, response) => {
-    response.send(`route is working!!`);
+// app.get('/location',(request, response) => {
+//     const url = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAW1TEC4aep3rrsRW9Z8EYiYNOC8d387v0&address=7600+wisconsin+ave+Bethesda+MD`
+//     superagent.get(url)
+//         .then(res => response.send(res.body))
+// })
+
+
+app.get('/location',(request, response) => {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAW1TEC4aep3rrsRW9Z8EYiYNOC8d387v0&address=7600+wisconsin+ave+Bethesda+MD`
+    superagent.get(url)
+        .then(res => response.send({
+            latitude: res.body.results[0].geometry.location.lat,
+            longitude: res.body.results[0].geometry.location.lng
+        }))
+    .catch(err => response.send('<img src="http://http.cat/404" />'))
+    
 })
 
 app.get(`*`, (request,response) => {
